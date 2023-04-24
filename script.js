c184460212719fe802bab87cb9bc287bc86abca9
Noel Ohaeri <noelohaeri@gmail.com>
2023-04-24 15:39:02 -0400

    Added functionality when you click on the clear button

--- script.js
+++ script.js
@@ -6,20 +6,24 @@ const selected = document.querySelector("#selected");
 const button = document.querySelector(".submit_btn");
 const pages = document.querySelector("#pages");
 const clearBtn = document.querySelector(".clear_btn");
+let ident = 0;
 
 function Book(title, author, pages, selected) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.selected = selected;
+  this.id = ident;
+  ident += 1;
+  console.log(ident);
 }
-
 
 function showLibrary() {
   for (i = 0; i < myLibrary.length; i++) {
     let book = myLibrary[i];
     const bookEl = document.createElement("tr");
     bookEl.className = "all";
+    bookEl.id = `book${book.id}`;
     bookEl.innerHTML = `<td> ${book.title} </td>
                             <td> ${book.author} </td>
                             <td> ${book.pages} </td>
@@ -27,7 +31,22 @@ function showLibrary() {
                             <td><button class="clrBtn"> Clear </button> </td>`;
 
     tableRow.appendChild(bookEl);
+
+    document
+      .querySelector(`#book${book.id} td button`)
+      .addEventListener("click", () => clearL(book.id));
   }
+}
+
+function clearL(bookId) {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    if (book.id === bookId) {
+      myLibrary.splice(i, 1);
+    }
+  }
+  clearLibrary();
+  showLibrary();
 }
 
 function clearLibrary() {
@@ -38,13 +57,19 @@ function clearLibrary() {
 }
 
 function addBookToLibrary() {
-  if (bookValue.value.length === 0 || authorValue.value.length === 0){
+  if (bookValue.value.length === 0 || authorValue.value.length === 0) {
     alert("Please, fill all the field");
     return;
   }
+
   clearLibrary();
 
-  const newBook = new Book(bookValue.value, authorValue.value, pages.value, selected.value);
+  const newBook = new Book(
+    bookValue.value,
+    authorValue.value,
+    pages.value,
+    selected.value
+  );
 
   myLibrary.push(newBook);
   showLibrary();
@@ -60,7 +85,6 @@ clearBtn.addEventListener("click", function () {
   myLibrary = [];
   clearLibrary();
 });
-
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 const gameOfThrones = new Book("Game of Thrones", "George", 300, true);
